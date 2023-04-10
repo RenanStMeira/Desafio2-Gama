7e8cf24b329ec6b1d318483e26e9957bd5629036
Renan <renan.meira25@gmail.com>
2023-04-10 14:35:14 -0300

    adicionando arquivos js

---
+++ JS/reservIngres.js
@@ -0,0 +1,10 @@
+/* Função para exibir o modal */
+function exibirModal() {
+    document.getElementById("modal").style.display = "block";
+  }
+  
+  /* Função para fechar o modal */
+  function fecharModal() {
+    document.getElementById("modal").style.display = "none";
+  }
+  
