c3e76986c8ddbd944bf17dfcc9642b16efbc7d5a
Renan <renan.meira25@gmail.com>
2023-04-16 21:41:24 -0300

    adicionando arquivos

--- JS/reservaIngres.js
+++ JS/reservaIngres.js
@@ -1,61 +1,122 @@
+
 const BASE_URL = 'https://soundgarden-api.vercel.app/bookings'
-const modal = document.getElementById("myModal");
-const btn = document.getElementById("openModalBtn");
-const fechar = document.getElementsByClassName("close")[0];
-const form = document.getElementById("reservationForm");
-const reserv = document.getElementById('reserv');
 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
 
-fechar.onclick = function() {
-  modal.style.display = "none";
-}
+const postsContainer = document.querySelector("#posts-container");
 
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+const postPage = document.querySelector("#post");
+const postContainer = document.querySelector("#post-container");
+const commentsContainer = document.querySelector("#comments-container");
 
-form.onsubmit = function(event) {
-  event.preventDefault(); 
-  // Obtem os dados do formulário
-  const nome = document.getElementById("nome").value;
-  const email = document.getElementById("email").value;
-  const tickets = document.getElementById("tickets").value;
+const commentForm = document.querySelector("#comment-form");
+const emailInput = document.querySelector("#email");
+const bodyInput = document.querySelector("#body");
+
+// Load post
+const urlSearchParams = new URLSearchParams(window.location.search);
+const postId = urlSearchParams.get("id");
+
+// Get all posts
+async function getAllPosts() {
+  const response = await fetch(url);
+
+  console.log(response);
+
+  const data = await response.json();
+
+  console.log(data);
+
  
-  // Cria um objeto com os dados do formulário
-  const formData = new FormData();
-  formData.append("nome", nome);
-  formData.append("email", email);
-  formData.append("tickets", tickets);
- 
-   
 
-  
-  // Envia os dados do formulário para o servidor usando fetch
-  fetch(BASE_URL, { 
-    mode: "no-cors",
-    method: "POST",
-    body: JSON.stringify(formData),
-    headers: { "Content-Type": "application/json" }
-  }) 
-  
-  .then(function(resposta) {
-    if (resposta.ok) {
-      // Se a resposta for bem-sucedida, você pode implementar a lógica de reserva do ingresso aqui
-      alert("Reserva de ingresso realizada com sucesso!");
-      modal.style.display = "none"; // Fecha o modal após o envio do formulário
-    } else {
-      alert("Erro ao fazer reserva de ingresso." );
-    }
-  })
-  .catch(function(error) {
-    alert("Erro ao fazer reserva de ingresso: " + error);
+  data.map((post) => {
+    const div = document.createElement("div");
+    const title = document.createElement("h2");
+    const body = document.createElement("p");
+    const link = document.createElement("a");
+
+    title.innerText = post.title;
+    body.innerText = post.body;
+    link.innerText = "en";
+    link.setAttribute("href", `/post.html?id=${post.id}`);
+
+    div.appendChild(title);
+    div.appendChild(body);
+    div.appendChild(link);
+    postsContainer.appendChild(div);
   });
 }
 
+// Get individual post
+async function getPost(id) {
+  const [responsePost, responseComments] = await Promise.all([
+    fetch(`${url}/${id}`),
+    fetch(`${url}/${id}/comments`),
+  ]);
+
+  const dataPost = await responsePost.json();
+
+  const dataComments = await responseComments.json();
 
 
+  postPage.classList.remove("hide");
+
+  const title = document.createElement("h1");
+  const body = document.createElement("p");
+
+  title.innerText = dataPost.title;
+  body.innerText = dataPost.body;
+
+  postContainer.appendChild(title);
+  postContainer.appendChild(body);
+
+  dataComments.map((comment) => {
+    createComment(comment);
+  });
+}
+
+function createComment(comment) {
+  const div = document.createElement("div");
+  const email = document.createElement("h3");
+  const commentBody = document.createElement("p");
+
+  email.innerText = comment.email;
+  commentBody.innerText = comment.body;
+
+  div.appendChild(email);
+  div.appendChild(commentBody);
+  commentsContainer.appendChild(div);
+}
+
+// Insert a comment
+async function postComment(comment) {
+  const response = await fetch(url, {
+    method: "POST",
+    body: comment,
+    headers: {
+      "Content-type": "application/json",
+    },
+  });
+
+  const data = await response.json();
+
+  createComment(data);
+}
+
+if (!postId) {
+  getAllPosts();
+} else {
+  getPost(postId);
+
+  commentForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let comment = {
+      email: emailInput.value,
+      body: bodyInput.value,
+    };
+
+    comment = JSON.stringify(comment);
+
+    postComment(comment);
+  });
+}
